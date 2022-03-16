a6f63a05e00154295b28e423126bf9ec7e1f1384
Joshua Payne <jpayne@434marketing.com>
2022-03-16 15:02:10 -0400

    remove gutenberg CSS

--- themes/accel434/functions.php
+++ themes/accel434/functions.php
@@ -64,6 +64,14 @@ function child_theme_setup() {
 	// For the Block Editor.
     add_theme_support( 'editor-styles' );
 
+    //Remove Gutenberg Block Library CSS from loading on the frontend
+    function smartwp_remove_wp_block_library_css(){
+        wp_dequeue_style( 'wp-block-library' );
+        wp_dequeue_style( 'wp-block-library-theme' );
+        wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+    } 
+    add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
 	// ** Backend **
 
 	// Meta Title - Move to the top
