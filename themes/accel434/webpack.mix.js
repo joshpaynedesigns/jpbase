164781dc98ff0f732377b8c29e8a2bfa6c3a215e
Eldon Yoder <eldonyoder@gmail.com>
2023-08-28 19:52:12 -0400

    copy svg files over to dist

--- themes/accel434/webpack.mix.js
+++ themes/accel434/webpack.mix.js
@@ -7,4 +7,6 @@ mix.js("assets/js/sitewide.js", "dist/front.js");
 mix.sass("assets/styles/style.scss", "dist/front.css");
 mix.sass("assets/styles/editor-style.scss", "dist/editor-style.css");
 
+mix.copy("assets/icons/src/*.svg", "dist/icons/");
+
 mix.sourceMaps(true, "source-map");
